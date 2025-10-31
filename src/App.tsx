f2b769daf0096c8aabbdf56849d6d60d759c14d0
Soareslili <lidianesantossoares2@gmail.com>
2025-10-31 08:18:14 -0300

    Criando sessao de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero"
 import Collection from "./components/Collection"
 import About from "./components/About"
 import Gallery from "./components/Gallery"
+import Lifestyle from "./components/Lifestyle"
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
    <About/>
     <Collection />
     <Gallery />
+    <Lifestyle />
    </>
     
       
---
+++ src/components/Lifestyle.tsx
@@ -0,0 +1,51 @@
+import EstiloImg from "../assets/Estilo.png"
+
+import { Button } from "./ui/button"
+
+
+const Lifestyle = () => {
+    return (
+        <section className="py-24 bg-black relative overflow-hidden">
+
+
+            <div className="luxury-container relative z-10">
+                <div className="grid lg:grid-cols-2 gap-20 items-center">
+
+                    <div className="relative">
+                        <div className="absolute inset-0 glow-gold blur-3xl opacity-20" />
+                        <img
+                            src={EstiloImg}
+                            alt="Watch Mechanism Details"
+                            className="relative z-10 rounded-lg w-full hover:scale-105 transition-transform duration-500"
+                        />
+                    </div>
+
+
+
+                    <div className="space-y-6">
+                        <h2 className="font-serif text-4xl md:text-6xl font-bold">
+                            Luxo não é uma opção  <span className="text-primary">é uma declaração.</span>
+                        </h2>
+                        <div className="h-1 w-24 bg-primary" />
+                        <p className="text-lg text-muted-foreground leading-relaxed">
+
+                            Mais do que um relógio, GetTime representa um compromisso com a excelência, precisão e estilo atemporal. Usado por quem entende que o verdadeiro luxo está nos detalhes.
+                        </p>
+                       
+
+                       <div className="flex justify-center lg:justify-start mt-6 ">
+                        <Button variant="hero" size="lg" className="text-lg px-12 cursor-pointer py-6 font-play h-auto">
+                          Explore Coleções
+                        </Button>
+                    </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+export default Lifestyle
